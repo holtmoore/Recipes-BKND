a0cf5f9bf31c2cf27723fde61b6887e2eb72214b
Holt Moore <holt.w.moore@gmail.com>
2023-11-18 12:37:50 -0600

    first/issues

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//index.js
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express({origin: "http://localhost:3000"});
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://doueven1996:h1kVUfRcnYY4b5ga@cluster0.b0gmr3u.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
